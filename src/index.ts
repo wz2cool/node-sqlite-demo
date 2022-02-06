18d2acc157bf56abba104d2cfcd150e7da07c257
wz2cool <wz2cool@live.cn>
2022-02-06 18:37:12 +0800

    add

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
+import sqlite3 from "sqlite3";
+
+const db = new sqlite3.Database(":memory:")
+
+db.serialize(function () {
+    db.run("CREATE TABLE lorem (info TEXT)");
+
+    var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+    for (var i = 0; i < 10; i++) {
+        stmt.run("Ipsum " + i);
+    }
+    stmt.finalize();
+
+    db.each("SELECT rowid AS id, info FROM lorem", function (err, row) {
+        console.log(row.id + ": " + row.info);
+    });
+});
+
+db.close();
+
 console.log("hello world");
